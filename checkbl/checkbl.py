abee31b1bcb7bcce788362d6ca45fbe1a12844d2
m0rphine28 <jbthomas28@gmail.com>
2022-09-13 16:39:21 +0200

    -

--- checkbl/checkbl.py
+++ checkbl/checkbl.py
@@ -22,109 +22,75 @@ databl=None
 list_geoip=None
 ###########################
 
+def get_ip_geoloc(ip):
+    global db
+    return str(db.lookup(str(ip)).country)
+
+
+def parse_fg_logs():
+    
+
+
+
+
+
 def run_bl_file(filename):
     global databl
     global db 
     global list_geoip
+    file_in = None
     retjson={'file_clean':False, 'GeoIP': {}, 'BL': {}}
     try:
         if str(filename).endswith(".gz"):
             #GZIP FILE
             #check ip line by line 
-            with gzip.open(filename) as file_in:
-                cnt=0
-                tmp_ipgeo={}
-                for line in file_in:
-                    cnt+=1
-                    linex=str(line.rstrip())
-                    ip = re.findall( r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b',linex)
-                    find_geo=None
-                    find_bl=False
-                    for ipx in ip:
-                        if not ipaddress.ip_address(str(ipx)).is_private:
-                            #Check in BL
-                            tmp_geo=None
-                            try:
-                                if ipx not in tmp_ipgeo:
-                                    tmp_ipgeo[ipx]=str(db.lookup(str(ipx)).country)
-                                    tmp_geo=tmp_ipgeo[ipx]
-                                else:
-                                    tmp_geo=tmp_ipgeo[ipx]
-                            except Exception as error:
-                                print('Error in db lookup on '+str(ipx)+' -> '+str(error))
-                                tmp_geo='Inconnu'
-                            if ipx in databl:
-                                find_bl=True
-                                if ipx not in retjson['BL']:
-                                    retjson['BL'][ipx]=databl[ipx]
-                                if ipx not in retjson['GeoIP']:
-                                    retjson['GeoIP'][ipx]=tmp_geo
-                                    find_geo=tmp_geo
-                                else:
-                                    find_geo=tmp_geo
-                            #Check in GeoIP
-                            elif list_geoip and tmp_geo not in list_geoip:
-                                if ipx not in retjson['GeoIP']:
-                                    retjson['GeoIP'][ipx]=tmp_geo
-                                    find_geo=tmp_geo
-                                else:
-                                    find_geo=tmp_geo
-                    if find_bl or find_geo:
-                        if 'lines_suspect' not in retjson:
-                            retjson['lines_suspect']=[]
-                            retjson['lines_suspect']=[]
-                        if find_bl:
-                            retjson['lines_suspect'].append({str(cnt): linex,'bl': True, 'geoip':find_geo})
-                        elif find_geo:
-                            retjson['lines_suspect'].append({str(cnt): linex,'bl': False, 'geoip':find_geo})
+            file_in = gzip.open(filename)
         else:
-            #check ip line by line 
-            with open(filename) as file_in:
-                cnt=0
-                tmp_ipgeo={}
-                for line in file_in:
-                    cnt+=1
-                    linex=str(line.rstrip())
-                    ip = re.findall( r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b',linex)
-                    find_geo=None
-                    find_bl=False
-                    for ipx in ip:
-                        if not ipaddress.ip_address(str(ipx)).is_private:
-                            #Check in BL
-                            tmp_geo=None
-                            try:
-                                if ipx not in tmp_ipgeo:
-                                    tmp_ipgeo[ipx]=str(db.lookup(str(ipx)).country)
-                                    tmp_geo=tmp_ipgeo[ipx]
-                                else:
-                                    tmp_geo=tmp_ipgeo[ipx]
-                            except Exception as error:
-                                print('Error in db lookup on '+str(ipx)+' -> '+str(error))
-                                tmp_geo='Inconnu'
-                            if ipx in databl:
-                                find_bl=True
-                                if ipx not in retjson['BL']:
-                                    retjson['BL'][ipx]=databl[ipx]
-                                if ipx not in retjson['GeoIP']:
-                                    retjson['GeoIP'][ipx]=tmp_geo
-                                    find_geo=tmp_geo
-                                else:
-                                    find_geo=tmp_geo
-                            #Check in GeoIP
-                            elif list_geoip and tmp_geo not in list_geoip:
-                                if ipx not in retjson['GeoIP']:
-                                    retjson['GeoIP'][ipx]=tmp_geo
-                                    find_geo=tmp_geo
-                                else:
-                                    find_geo=tmp_geo
-                    if find_bl or find_geo:
-                        if 'lines_suspect' not in retjson:
-                            retjson['lines_suspect']=[]
-                            retjson['lines_suspect']=[]
-                        if find_bl:
-                            retjson['lines_suspect'].append({str(cnt): linex,'bl': True, 'geoip':find_geo})
-                        elif find_geo:
-                            retjson['lines_suspect'].append({str(cnt): linex,'bl': False, 'geoip':find_geo})
+            file_in = open(filename)
+        cnt=0
+        tmp_ipgeo={}
+        for line in file_in:
+            cnt+=1
+            linex=str(line.rstrip())
+            ip = re.findall( r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b',linex)
+            find_geo=None
+            find_bl=False
+            for ipx in ip:
+                if not ipaddress.ip_address(str(ipx)).is_private:
+                    #Check in BL
+                    tmp_geo=None
+                    try:
+                        if ipx not in tmp_ipgeo:
+                            tmp_ipgeo[ipx] = get_ip_geoloc(ipx)
+                            tmp_geo = tmp_ipgeo[ipx]
+                        else:
+                            tmp_geo=tmp_ipgeo[ipx]
+                    except Exception as error:
+                        print('Error in db lookup on '+str(ipx)+' -> '+str(error))
+                        tmp_geo='Inconnu'
+                    if ipx in databl:
+                        find_bl=True
+                        if ipx not in retjson['BL']:
+                            retjson['BL'][ipx]=databl[ipx]
+                        if ipx not in retjson['GeoIP']:
+                            retjson['GeoIP'][ipx]=tmp_geo
+                            find_geo=tmp_geo
+                        else:
+                            find_geo=tmp_geo
+                    #Check in GeoIP
+                    elif list_geoip and tmp_geo not in list_geoip:
+                        if ipx not in retjson['GeoIP']:
+                            retjson['GeoIP'][ipx]=tmp_geo
+                            find_geo=tmp_geo
+                        else:
+                            find_geo=tmp_geo
+            if find_bl or find_geo:
+                if 'lines_suspect' not in retjson:
+                    retjson['lines_suspect']=[]
+                if find_bl:
+                    retjson['lines_suspect'].append({str(cnt): linex,'bl': True, 'geoip':find_geo})
+                elif find_geo:
+                    retjson['lines_suspect'].append({str(cnt): linex,'bl': False, 'geoip':find_geo})
     except Exception as e:
         retjson['error']=str(e)
     if not 'lines_suspect' in retjson:
